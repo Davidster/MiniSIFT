3febc7574bd1c1bb025c06cf2481fa86cc6e739c
David Huculak <davidhuculak5@gmail.com>
2021-09-25 13:47:13 -0400

    dummy

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,7 @@
 use image::DynamicImage;
 use image::GrayImage;
 use image::Luma;
+use image::Rgb;
 use image::RgbImage;
 use show_image::WindowProxy;
 use std::sync::mpsc::channel;
@@ -9,6 +10,13 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
+
+struct KeyPoint {
+    x: u32,
+    y: u32,
+    radius: f32,
+    color: [u8; 3],
+}
 
 struct SplitImage {
     r: GrayImage,
@@ -90,6 +98,17 @@ fn get_image_gradients(img: &GrayImage) -> ImageGradients {
     }
 }
 
+fn draw_keypoints(img: &RgbImage, keypoints: &Vec<KeyPoint>) -> RgbImage {
+    let mut result = RgbImage::new(img.width(), img.height());
+    for (x, y, _) in img.enumerate_pixels() {
+        result.put_pixel(x, y, img.get_pixel(x, y).clone());
+    }
+    for keypoint in keypoints {
+        result.put_pixel(keypoint.x, keypoint.y, Rgb::from(keypoint.color));
+    }
+    result
+}
+
 #[show_image::main]
 fn main() {
     let mut windows: Vec<WindowProxy> = Vec::new();
@@ -98,6 +117,36 @@ fn main() {
         load_rgb_image_from_file("../featureMatcher/image_sets/panorama/pano1_0008.png");
     let pixel = building_img.get_pixel(building_img.width() - 1, building_img.height() - 1);
     println!("{}, {}, {}", pixel[0], pixel[1], pixel[2]);
+
+    let building_img_w_keypoints = draw_keypoints(
+        &building_img,
+        &vec![
+            KeyPoint {
+                x: 10,
+                y: 10,
+                radius: 0f32,
+                color: [255, 0, 0],
+            },
+            KeyPoint {
+                x: 100,
+                y: 10,
+                radius: 0f32,
+                color: [0, 255, 0],
+            },
+            KeyPoint {
+                x: 100,
+                y: 100,
+                radius: 0f32,
+                color: [0, 0, 255],
+            },
+            KeyPoint {
+                x: 10,
+                y: 100,
+                radius: 0f32,
+                color: [255, 0, 255],
+            },
+        ],
+    );
 
     let SplitImage {
         g: building_img_g,
@@ -110,6 +159,10 @@ fn main() {
     } = get_image_gradients(&building_img_g);
 
     windows.push(show_rgb_image(building_img.clone(), "Building"));
+    windows.push(show_rgb_image(
+        building_img_w_keypoints.clone(),
+        "Building w keypoints",
+    ));
     windows.push(show_grayscale_image(
         building_img_g.clone(),
         "Building Green",
